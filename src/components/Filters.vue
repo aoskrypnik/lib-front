c2bd52deeb6b84b1890c43a6d10be11e31417b0a
andrew <andrii.skrypnik@gmail.com>
2021-03-05 14:02:33 +0200

    Initial commit

---
+++ src/components/Filters.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Фільтри
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-checkbox
+            v-model="available_only"
+            label="Тільки в наявності"
+        >
+        </v-checkbox>
+        <v-autocomplete
+            v-model="selectedAuthors"
+            :items="authorNames"
+            label="Автори"
+            outlined
+            chips
+            small-chips
+            multiple
+        ></v-autocomplete>
+
+        <v-autocomplete
+            v-model="selectedGenres"
+            :items="genreNames"
+            label="Genres"
+            outlined
+            chips
+            small-chips
+            multiple
+        ></v-autocomplete>
+
+        <v-btn text block color="primary">
+          Застосувати фільтри
+        </v-btn>
+      </v-form>
+    </v-card-text>
+
+  </v-card>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+  name: "Filters",
+  data() {
+    return {
+      available_only: false,
+      selectedAuthors: [],
+      selectedGenres: [],
+    }
+  },
+
+  computed: {
+    ...mapGetters([
+      'genresGetter',
+      'authorsGetter'
+    ]),
+    genreNames: function () {
+      return this.genresGetter.map(genre => genre.genreName)
+    },
+    authorNames: function () {
+      return this.authorsGetter.map(author => author.authorName)
+    }
+  },
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
